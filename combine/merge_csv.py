95ede4c0bb0cbf7871aac66fbee1331f792e8e2f
Parth Diwane <parth@Parths-MacBook-Pro.local>
2025-07-21 08:48:25 -0700

    cleaned up data a little and fully fixed the submodule issue. a new directory was also made --> singles net_stats --> allows us to more easily view the csv file and work w/ it in the future. we are are going to need to change some of the paths in the jupyter notebook files tho

--- combine/merge_csv.py
+++ combine/merge_csv.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 merge = pd.DataFrame()
 for i in range(57):
-    df = pd.read_csv('stats/tennis_atp-master_'+ str(1968 + i) + '.csv')
+    df = pd.read_csv('stats/tennis_atp/atp_matches_'+ str(1968 + i) + '.csv')
     
     merge = pd.concat([merge,df], ignore_index=True) # combines independent csv files into one data frame
 
-merge.to_csv('/Users/parth/coding/python/sportly/stats/tennis_atp/singles/atp_matches_net.csv') # converts data frame into csv file for later use
+merge.to_csv('/Users/parth/coding/sportly/stats/singles_net_stats/singles_net_stats.csv') # converts data frame into csv file for later use
--- combine/remove.py
+++ combine/remove.py
@@ -1,7 +1,7 @@
 # reminder : change ts to local not abs path 
 import pandas as pd
 
-df = pd.read_csv('/Users/parth/coding/python/sportly/stats/tennis_atp/singles/atp_matches_net.csv') # net csv file for singles (no doubles files yet)
+df = pd.read_csv('stats/singles_net_stats/singles_net_stats.csv') # net csv file for singles (no doubles files yet)
  
 df = df.drop(columns=['tourney_id','tourney_date','winner_id','loser_id','tourney_date','match_num']) # get rid of unnecessary columns in df
-df.to_csv('/Users/parth/coding/python/sportly/stats/tennis_atp/singles/atp_matches_net.csv')
+df.to_csv('stats/singles_net_stats/singles_net_stats.csv')
