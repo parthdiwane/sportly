31a618a3ad93c1b2caa853848eff47c3ddf26787
Parth Diwane <parth@Parths-MacBook-Pro.local>
2025-07-21 08:35:42 -0700

    maybe fixed data issue --> tennis atp sub module issue

--- combine/remove.py
+++ combine/remove.py
@@ -1,6 +1,7 @@
+# reminder : change ts to local not abs path 
 import pandas as pd
 
-df = pd.read_csv('/Users/parth/coding/python/sportly/stats/tennis_atp/singles/atp_matches_net.csv')
+df = pd.read_csv('/Users/parth/coding/python/sportly/stats/tennis_atp/singles/atp_matches_net.csv') # net csv file for singles (no doubles files yet)
  
-df = df.drop(columns=['tourney_id','tourney_date','winner_id','loser_id','tourney_date','match_num'])
+df = df.drop(columns=['tourney_id','tourney_date','winner_id','loser_id','tourney_date','match_num']) # get rid of unnecessary columns in df
 df.to_csv('/Users/parth/coding/python/sportly/stats/tennis_atp/singles/atp_matches_net.csv')
